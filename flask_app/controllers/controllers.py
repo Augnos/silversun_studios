f2d79b6b2aee14e5976016a31605012da06aa40c
Joshua Valdez <josh@augnos.com>
2022-10-14 09:16:56 -0700

    Started building templates

---
+++ flask_app/controllers/controllers.py
@@ -0,0 +1,79 @@
+from flask import render_template, redirect, request
+from flask_app import app
+from flask_bcrypt import Bcrypt
+from flask_app.models.model import Model
+
+
+# -----------------------------------------------------
+# -------------------- GET Routes ---------------------
+# -----------------------------------------------------
+
+# ---------- Home Page -----------
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+# ---------- Models (all) Page -----------
+@app.route("/models")
+def models():
+    return render_template("models.html", all_models = Model.select_all_models())
+
+
+# ---------- Models (one) Page -----------
+@app.route("/models/<id>")
+def models_one(id):
+    return render_template("show_model.html", model = Model.get_one({"id":id}))
+
+
+# ------------------------------------------------------
+# -------------------- POST Routes ---------------------
+# ------------------------------------------------------
+
+# ---------- Insert New Model Post -----------
+@app.route('/insert/model', methods=["POST"])
+def insert_model():
+    data = {
+        "name": request.form["name"],
+    }
+    Model.insert_new_model(data)
+    return redirect('/')
+
+
+# ---------- Update Model Post -----------
+@app.route('/models/<id>/update', methods=["POST"])
+def edit_model(id):
+    data = {
+        "id": id,
+        "name": request.form["name"],
+    }
+    Model.update_model(data)
+    return redirect('/')
+
+
+# ---------- Delete Model Post -----------
+@app.route('/models/<id>/delete')
+def delete_model(id):
+
+    data = {"id": id}
+    Model.delete(data)
+    return redirect('/')
+
+
+# # ---------- Bcrypt Password Post -----------
+# @app.route('/register/user', methods=['POST'])
+# def register():
+#     # validate the form here ...
+#     # create the hash
+#     pw_hash = bcrypt.generate_password_hash(request.form['password'])
+#     print(pw_hash)
+#     # put the pw_hash into the data dictionary
+#     data = {
+#         "username": request.form['username'],
+#         "password" : pw_hash
+#     }
+#     # Call the save @classmethod on User
+#     user_id = User.save(data)
+#     # store user id into session
+#     session['user_id'] = user_id
+#     return redirect("/dashboard")
---
+++ flask_app/models/model.py
@@ -0,0 +1,62 @@
+from flask import flash
+from flask_app.config.mysqlconnection import connectToMySQL
+
+
+class Model:
+    def __init__(self, data):
+        self.id = data['id']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+
+    # ---------- Select All Models -----------
+    @classmethod
+    def select_all_models(cls):
+        query = "SELECT * FROM models;"
+        results = connectToMySQL('schema').query_db(query)
+        models = []
+        for model in results:
+            models.append(cls(model))
+        return models
+
+
+    # ---------- Select Most Recent Model -----------
+    @classmethod
+    def select_recent_model(cls):
+        query = "SELECT * FROM models ORDER BY id DESC LIMIT 1;"
+        results = connectToMySQL('schema').query_db(query)
+        models = []
+        for model in results:
+            models.append(cls(model))
+        return models
+
+
+    # ---------- Insert New Model -----------
+    @classmethod
+    def insert_model(cls, data):
+        query = "INSERT INTO models ( created_at, updated_at ) VALUES ( NOW() , NOW() );"
+        return connectToMySQL('schema').query_db(query, data)
+
+
+    # ---------- Update Model -----------
+    @classmethod
+    def update_model(cls, data):
+        query = "UPDATE models SET updated_at = NOW() WHERE id = %(id)s;"
+        return connectToMySQL('schema').query_db(query, data)
+
+
+    # ---------- Delete Model -----------
+    @classmethod
+    def delete_model(cls, data):
+        query = "DELETE FROM models WHERE id = %(id)s;"
+        return connectToMySQL('schema').query_db(query, data)
+
+
+    # ---------- Validation -----------
+    @staticmethod
+    def is_valid(survey):
+        is_valid = True
+        if survey == False:
+            is_valid = False
+            flash("Survey must be true!")
+        return is_valid

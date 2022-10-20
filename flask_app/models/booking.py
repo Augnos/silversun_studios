72c4e72f07f084e7f29ce65c6912fe4557a1f7d4
Joshua Valdez <josh@augnos.com>
2022-10-20 14:36:10 -0700

    Removing unused bootstrap node downloads

--- flask_app/models/booking.py
+++ flask_app/models/booking.py
@@ -1,24 +1,28 @@
 from flask import flash
 from flask_app.config.mysqlconnection import connectToMySQL
 
+studio_a_id = "c_1887fvbjsimgghkbmqeu2kirblq9q@resource.calendar.google.com"
+studio_b_id = "c_188bk9jmjf75uge4g4319luuql6q8@resource.calendar.google.com"
+studio_c_id = "c_18896sfskcn9mg25l00tet4jannn2@resource.calendar.google.com"
+live_room_id = "c_1885ahjvknvmogg9ldra4ls1m37fc@resource.calendar.google.com"
+timeZone = "America/Los_Angeles"
 
 class Booking:
     def __init__(self, data):
         self.id = data['id']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
+        self.calendarId = data['calendarId']
+        self.summary = data['summary']
+        self.description = data['description']
+        self.start = data['start']
+        self.end = data['end']
+        self.attendees = data['attendees']
 
 
     # ---------- Get All Bookings -----------
     @classmethod
     def select_all_bookings(cls):
-        query = "SELECT * FROM bookings;"
-        results = connectToMySQL('schema').query_db(query)
-        bookings = []
-        for booking in results:
-            bookings.append(cls(booking))
-        return bookings
-
+        event = service.events().get(calendarId='primary', eventId='eventId').execute()
+        print event['summary']
 
     # ---------- Insert New Booking -----------
 #     ---psuedocode---

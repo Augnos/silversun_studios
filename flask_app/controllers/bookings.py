a358d394bd55a5ec4cf1b50e1f5a621b1cbd5417
Joshua Valdez <josh@augnos.com>
2022-10-20 22:52:06 -0700

    Attempts at gcal events

--- flask_app/controllers/bookings.py
+++ flask_app/controllers/bookings.py
@@ -1,6 +1,5 @@
 from flask import render_template, redirect, request
 from flask_app import app
-from flask_bcrypt import Bcrypt
 from flask_app.models.booking import Booking
 
 
--- flask_app/models/booking.py
+++ flask_app/models/booking.py
@@ -1,6 +1,18 @@
-from flask import flash
-from flask_app.config.mysqlconnection import connectToMySQL
+# from flask_app.config.mysqlconnection import connectToMySQL
+from pprint import pprint
+from flask_app.models.Google import create_service, convert_to_RFC_datetime
 
+CLIENT_SECRET_FILE = 'flask_app/config/credentials.json'
+API_NAME = 'calendar'
+API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+service = create_service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+ 
+studio_a_id = "c_1887fvbjsimgghkbmqeu2kirblq9q@resource.calendar.google.com"
+studio_b_id = "c_188bk9jmjf75uge4g4319luuql6q8@resource.calendar.google.com"
+studio_c_id = "c_18896sfskcn9mg25l00tet4jannn2@resource.calendar.google.com"
+live_room_id = "c_1885ahjvknvmogg9ldra4ls1m37fc@resource.calendar.google.com"
 
 class Booking:
     def __init__(self, data):
@@ -12,10 +24,7 @@ class Booking:
         self.end = data['end']
         self.attendees = data['attendees']
 
-    studio_a_id = "c_1887fvbjsimgghkbmqeu2kirblq9q@resource.calendar.google.com"
-    studio_b_id = "c_188bk9jmjf75uge4g4319luuql6q8@resource.calendar.google.com"
-    studio_c_id = "c_18896sfskcn9mg25l00tet4jannn2@resource.calendar.google.com"
-    live_room_id = "c_1885ahjvknvmogg9ldra4ls1m37fc@resource.calendar.google.com"
+
 
 
     # ---------- Get All Bookings -----------
@@ -33,5 +42,22 @@ class Booking:
 #         If no conflicts, create new event with title as user's name.
     @classmethod
     def insert_booking(cls, data):
+        colors = service.colors().get().execute()
+        pprint(color)
+        
+        hour_adjustment = -8
+        event_request_body = {
+            
+            'start': {
+                'dateTime': convert_to_RFC_datetime(2022, 8, 1, 12 + hour_adjustment, 30),
+                'timeZone': 'Asia/Taipei'
+            },
+            'end': {
+                'dateTime': convert_to_RFC_datetime(2022, 8, 1, 12 + hour_adjustment, 30),
+                'timeZone': 'Asia/Taipei'
+            },
+            
+        }
+        
         query = "INSERT INTO bookings ( created_at, updated_at ) VALUES ( NOW() , NOW() );"
         return connectToMySQL('schema').query_db(query, data)

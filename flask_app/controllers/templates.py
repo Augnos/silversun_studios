59291e63d156f2399eff97e251a3c50fd17d0319
Joshua Valdez <josh@augnos.com>
2022-11-16 12:34:11 -0800

    Added meta tags, template partials

--- flask_app/controllers/templates.py
+++ flask_app/controllers/templates.py
@@ -5,40 +5,40 @@ from flask_app import app
 # ---------- Home Page -----------
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', home=True)
 
 
 # ---------- About Page -----------
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', about=True)
 
 
 # ---------- Studios Page -----------
 @app.route("/studios")
 def studios():
-    return render_template('studios.html')
+    return render_template('studios.html', studios=True)
 
 
 # ---------- Services Page -----------
 @app.route("/services")
 def services():
-    return render_template('services.html')
+    return render_template('services.html', services=True)
 
 
 # ---------- FAQ Page -----------
 @app.route("/faq")
 def faq():
-    return render_template('faq.html')
+    return render_template('faq.html', faq=True)
 
 
 # ---------- Availability Page -----------
 @app.route("/availability")
 def availability():
-    return render_template('availability.html')
+    return render_template('availability.html', availability=True)
 
 
 # ---------- Booking Page -----------
 @app.route("/booking")
 def booking():
-    return render_template('booking.html')
+    return render_template('booking.html', booking=True)
